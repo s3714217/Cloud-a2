f40c48b5052fda23099f2eb9adb8c02376b1fe2f
Thien Nguyen <41944095+s3714217@users.noreply.github.com>
2020-04-11 19:49:53 +1000

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,54 @@
 <?php
-
+session_start();
 /*
 https://github.com/GoogleCloudPlatform/php-docs-samples
 index.php file is created to work php76
 */
-switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
-    case '/':
-        require 'frontend/main.html';
-        break;
-    default:
-        http_response_code(404);
-        exit('Not Found');
+
+if(isset($_SESSION["username"]))
+{
+    switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+        case '/':
+            require 'login.html';
+            break;
+        case '/register.html':
+            require 'register.html';
+            break;
+        case '/main.html':
+            require 'main.html';
+            break;
+        case '/post.html':
+            require 'post.html';
+            break;
+        case '/profile.html':
+            require 'profile.html';
+            break;
+        case '/search.html':
+            require 'search.html';
+            break;
+        case '/product.html':
+            require 'product.html';
+            break;
+        case '/trade.html':
+            require 'trade.html';
+            break;
+        default:
+            http_response_code(404);
+            exit('Not Found');
+    }
+}
+else
+{
+    switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
+        case '/':
+            require 'login.html';
+            break;
+        case '/register.html':
+            require 'register.html';
+            break;
+        default:
+            http_response_code(404);
+            exit('Not Found');
+        }
+        
 }
